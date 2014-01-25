ef434f56bf4da20ff99aa14fb0ce41c59933eb96
lamkeewei <keewei.lam.2012@sis.smu.edu.sg>
2014-01-25 14:27:42 +0800

    Set up path that trigger ogr2ogr

--- app.js
+++ app.js
@@ -4,33 +4,31 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
 var path = require('path');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.use(express.static(path.join(__dirname, 'public')));
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+app.get('/ogr', function(req, res){
+  exec('ogr2ogr', function(error, stdout, stderr){
+    console.log(stdout);
+    res.send('hello');
+  });
 });
+
+app.listen(app.get('port'));
